9ce307bdbebd99a427a764cc6239d46233ba137f
Harrison Malone <harrison.malone2@gmail.com>
2018-05-11 16:54:18 +1000

    finished basic running calculator

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,11 +8,35 @@ class QuestionsController < ApplicationController
   def pace_answer
     @minutes = params[:minutes].to_i
     @seconds = params[:seconds].to_i
-    @distance = params[:distance].to_f
+    distance = params[:distance].to_f
     total_seconds = seconds_conversion(@minutes, @seconds)
-    total = total_seconds / @distance
-    @minutes = total / 60
-    @minutes = @minutes.round
+    total = total_seconds / distance
+    total = total / 60
+    @minutes = total.round
+    @seconds = total % 1
+    @seconds = @seconds * 60
+    @seconds = @seconds.round
+  end
+
+  def distance_answer
+    minutes = params[:minutes].to_i
+    seconds = params[:seconds].to_i
+    total_seconds = seconds_conversion(minutes, seconds)
+    pace_minutes = params[:pace_minutes].to_i
+    pace_seconds = params[:pace_seconds].to_i
+    total_pace_seconds = seconds_conversion(pace_minutes, pace_seconds)
+    @total = total_seconds.to_f / total_pace_seconds
+    @total = @total.round(2)
+  end
+
+  def time_answer
+    pace_minutes = params[:pace_minutes].to_i
+    pace_seconds = params[:pace_seconds].to_i
+    total_pace_seconds = seconds_conversion(pace_minutes, pace_seconds)
+    distance = params[:distance].to_f
+    total = total_pace_seconds * distance
+    total = total / 60
+    @minutes = total.round
     @seconds = total % 1
     @seconds = @seconds * 60
     @seconds = @seconds.round
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'time_calculator', to: 'questions#time_calculator', as: :time_calculator
   get 'pace_answer', to: 'questions#pace_answer', as: :pace_answer
+  get 'distance_answer', to: 'questions#distance_answer', as: :distance_answer
+  get 'time_answer', to: 'questions#time_answer', as: :time_answer
 end
